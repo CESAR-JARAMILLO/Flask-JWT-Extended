8e6d5a0c5feb296af5b6edefbcaac03d26c81c21
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-23 21:54:15 -0700

    added token refresh for REST API

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_refresh_token_required,
+    get_jwt_identity
+)
 from models.user import UserModel
 
 _user_parser = reqparse.RequestParser()
@@ -59,7 +64,7 @@ class UserLogin(Resource):
         # this is what the `authenticate()` function did in security.py
         if user and safe_str_cmp(user.password, data['password']):
             # identity= is what the identity() function did in security.py—now stored in the JWT
-            access_token = create_access_token(identity=user.id, fresh=True) 
+            access_token = create_access_token(identity=user.id, fresh=True)
             refresh_token = create_refresh_token(user.id)
             return {
                 'access_token': access_token,
@@ -67,3 +72,10 @@ class UserLogin(Resource):
             }, 200
 
         return {"message": "Invalid Credentials!"}, 401
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
